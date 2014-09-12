bc076a38087fb8adbcaa442264aaa0b9405b70fb
Matt Taylor <Matt@Navgear.com>
2014-09-12 16:19:23 -0500

    created initial rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '40249830a9373e4c7648897e8833ae1c8453e935cbdcf3a2e04af14c9ee10354a275b11bd741e9204b51bfd48f0a02c95198e4fe35deb2accedbfcab3ff2c918'
